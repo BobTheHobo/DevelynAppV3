96fd76137ac4d8822a17687cf26130eaa3f0633f
Viet <thienvietngomai@gmail.com>
2019-01-04 22:47:31 -0700

    Added "Add to homescreen" prompt

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -412,7 +412,7 @@ initFirebaseAuth();
 //stuff for web app support
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('./worker.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
@@ -421,3 +421,37 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+
+var btnAdd = document.getElementById('butPrompt');
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  	// Prevent Chrome 67 and earlier from automatically showing the prompt
+  	e.preventDefault();
+  	// Stash the event so it can be triggered later.
+  	deferredPrompt = e;
+   // Update UI notify the user they can add to home screen
+  	btnAdd.style.display = 'block';
+});
+
+btnAdd.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
+
+window.addEventListener('appinstalled', (evt) => {
+  app.logEvent('a2hs', 'installed');
+});
--- public/worker.js
+++ public/worker.js
@@ -1,16 +1,21 @@
+
 // use a cacheName for cache versioning
 var cacheName = 'v1:static';
+var urlsToCache = [
+    './',
+    './styles/main.css',
+    './images/develyn-logo192.png',
+    './images/develyn-logo512.png'
+    //'./script/main.js' --this will create an infinite loop btw
+]
 
 // during the install phase you usually want to cache static assets
 self.addEventListener('install', function(e) {
     // once the SW is installed, go ahead and fetch the resources to make this work offline
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
-            return cache.addAll([
-              '/',
-              '/styles/main.css',
-              '/script/main.js'
-            ]).then(function() {
+            console.log('Opened Cache')
+            return cache.addAll(urlsToCache).then(function() {
                 self.skipWaiting();
             });
         })
